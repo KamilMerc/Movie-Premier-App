7242976c7ae9e8cf2e3b0dc4298da61c327ec840
Kamil <k.merc@op.pl>
2023-11-02 22:26:18 +0100

    Re-style header form

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,56 +28,58 @@ const [filter, setFilter] = useState("")
           <h1>Movie Premier App</h1>
           <form>
             {/* Genre selection */}
-            <label htmlFor="genres">Genre:</label>
-            <select 
-              id="genres"
-              name="genres"
-              onChange={(e) => {
-                setGenre(`&with_genres=${e.target.value}`)
-              }}
-            >
-              {/* Genre options */}
-              <option value="">All</option>
-              <option value="28">Action</option>
-              <option value="12">Adventure</option>
-              <option value="16">Animation</option>
-              <option value="35">Comedy</option>
-              <option value="80">Crime</option>
-              <option value="99">Documentary</option>
-              <option value="18">Drama</option>
-              <option value="10751">Family</option>
-              <option value="14">Fantasy</option>
-              <option value="36">History</option>
-              <option value="27">Horror</option>
-              <option value="10402">Music</option>
-              <option value="9648">Mystery</option>
-              <option value="10749">Romance</option>
-              <option value="878">Science Fiction</option>
-              <option value="10770">TV Movie</option>
-              <option value="53">Thriller</option>
-              <option value="10752">War</option>
-              <option value="37">Western</option>
-            </select>
-
-
-            <label htmlFor="sortby">Sort by:</label>
-            <select 
-              id="sortby"
-              name="sortby"
-              onChange={(e) => {
-                setFilter(`&sort_by=${e.target.value}`)
-              }}
-            >
-              {/* sortby options */}
-              <option value="popularity.desc">Popularity DESC</option>
-              <option value="popularity.asc">Popularity ASC</option>
-              <option value="vote_average.desc">Vote average DESC</option>
-              <option value="vote_average.asc">Vote average ASC</option>
-              <option value="primary_release_date.desc">Release date DESC</option>
-              <option value="primary_release_date.asc">Release date ASC</option>
-            </select>
+            <div className="filters-container">
+              <label htmlFor="genres">Genre:</label>
+              <select 
+                id="genres"
+                name="genres"
+                onChange={(e) => {
+                  setGenre(`&with_genres=${e.target.value}`)
+                }}
+              >
+                {/* Genre options */}
+                <option value="">All genres</option>
+                <option value="28">Action</option>
+                <option value="12">Adventure</option>
+                <option value="16">Animation</option>
+                <option value="35">Comedy</option>
+                <option value="80">Crime</option>
+                <option value="99">Documentary</option>
+                <option value="18">Drama</option>
+                <option value="10751">Family</option>
+                <option value="14">Fantasy</option>
+                <option value="36">History</option>
+                <option value="27">Horror</option>
+                <option value="10402">Music</option>
+                <option value="9648">Mystery</option>
+                <option value="10749">Romance</option>
+                <option value="878">Science Fiction</option>
+                <option value="10770">TV Movie</option>
+                <option value="53">Thriller</option>
+                <option value="10752">War</option>
+                <option value="37">Western</option>
+              </select>
+            </div>
+            
+            <div className="filters-container">
+              <label htmlFor="sortby">Sort by:</label>
+              <select 
+                id="sortby"
+                name="sortby"
+                onChange={(e) => {
+                  setFilter(`&sort_by=${e.target.value}`)
+                }}
+              >
+                {/* sortby options */}
+                <option value="popularity.desc">Popularity DESC</option>
+                <option value="popularity.asc">Popularity ASC</option>
+                <option value="vote_average.desc">Vote average DESC</option>
+                <option value="vote_average.asc">Vote average ASC</option>
+                <option value="primary_release_date.desc">Release date DESC</option>
+                <option value="primary_release_date.asc">Release date ASC</option>
+              </select>
+              </div>
           </form>
-
         </div>
       </header>
     )
