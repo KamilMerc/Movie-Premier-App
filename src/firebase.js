5fad33375ed125ec4717546bc597d4d7f83282cc
Kamil <k.merc@op.pl>
2023-11-24 14:30:31 +0100

    Update firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
 
 //Firebase configuration
 const firebaseConfig = {
@@ -13,4 +14,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app)
 export default app
