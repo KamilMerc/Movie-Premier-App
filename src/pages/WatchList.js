66adfb6fedf57f37948a1210499d31ea2e543363
Kamil <k.merc@op.pl>
2023-11-30 21:54:03 +0100

    Add back to home page link in watch list page

--- src/pages/WatchList.js
+++ src/pages/WatchList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SavedMovies from "../components/SavedMovies";
+import { Link } from "react-router-dom";
 
 const WatchList = () => {
 
     return(
         <>
             <div className="watchListWrapper">
-                <h1 className="title">Watch list</h1>
+                <div className="wishListHeader">
+                    <h1 className="title">Watch list</h1>
+                    <Link to={"/"} style={{textDecoration:"none",color:"white"}}><p className="homePage">Back to home page</p></Link>
+                </div>
                 <div className="container center watchList">
                     <SavedMovies/>
                 </div>
